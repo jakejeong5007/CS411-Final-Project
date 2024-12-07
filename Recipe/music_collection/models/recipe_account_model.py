81e1d7c73b6d760b6cb55602719c282b9eacb13c
jakejeong <jake50071227@gmail.com>
2024-12-07 12:19:32 -0500

    temp file added

---
+++ Recipe/music_collection/models/recipe_account_model.py
@@ -0,0 +1,7 @@
+"""
+File that handles functions like 
+
+save -> Saves recipe to user's profie
+preferences -> sets selected recipes to prefered recipe for specific user
+get_preferences -> gets current preference
+"""
